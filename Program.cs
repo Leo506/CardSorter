4d491d1df48617d53a30604864b8242a4943b1fb
Artem Levin <leoartemlev@yandex.ru>
2022-05-20 22:46:45 +0500

    Дописал демонстрацию

--- Program.cs
+++ Program.cs
@@ -6,16 +6,31 @@ public class Program
     {
         DeckManager manager = new DeckManager();
 
+        Console.WriteLine("Adding new deck called \"First\"");
         manager.CreateNewDeck("First");
 
+        Console.WriteLine("All names:");
+        foreach (var item in manager.GetAllNames())
+            Console.WriteLine(item);
+
+        Console.WriteLine("Show full deck called \"First\"");
         foreach (var item in manager.GetDeck("First"))
             Console.WriteLine(item);
 
         Console.WriteLine("-------------------------------");
 
+        
         manager.ShuffleDeck("First");
 
+        Console.WriteLine("Show shuffled deck:");
         foreach (var item in manager.GetDeck("First"))
+            Console.WriteLine(item);
+
+        Console.WriteLine("Removing deck called \"First\"");
+        manager.RemoveDeck("First");
+
+        Console.WriteLine("All names:");
+        foreach (var item in manager.GetAllNames())
             Console.WriteLine(item);
     }
 }
