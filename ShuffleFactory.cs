637164ba845d262fe650137ea476bd2ccbc1e468
Artem Levin <leoartemlev@yandex.ru>
2022-05-20 22:19:09 +0500

    Реализовал смену перетасовщика через файл конфигурации

--- ShuffleFactory.cs
+++ ShuffleFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,16 @@ namespace TestTask.Shuffle
 {
     public class ShuffleFactory
     {
-        public static IShuffler GetShuffler() => new SimpleShuffler();
+        public static IShuffler GetShuffler()
+        {
+            var reader = new AppSettingsReader();
+
+            var shufflerSetting = reader.GetValue("Shuffler", typeof(string));
+
+            if (shufflerSetting != null && shufflerSetting.ToString() == "Human")
+                return new HumanShuffler();
+
+            return new SimpleShuffler();
+        }
     }
 }
