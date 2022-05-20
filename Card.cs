4c1b4d19f13c97b7880ee272e4b8da8a40646fbf
Artem Levin <leoartemlev@yandex.ru>
2022-05-20 21:21:29 +0500

    Написал класс колоды и структуру карты

---
+++ Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Deck
+{
+    /// <summary>
+    /// Структура, представляющая карту
+    /// </summary>
+    public struct Card
+    {
+        public string Value { get; set; }  // Достоинство карты
+        public string Suit { get; set; }   // Масть карты
+
+        public Card(string value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Deck
+{
+    public class Deck
+    {
+        private static readonly string[] valueVariants = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "B", "D", "K", "T"};
+        private static readonly string[] suitsVariants = { "Heart", "Spades", "Clubs", "Diamonds" };
+
+        private Card[] cards;  // Массив карт - это и есть колода
+
+        public string DeckName { get; private set; }
+
+
+        /// <summary>
+        /// Создаёт новую колоду
+        /// </summary>
+        /// <param name="name">Название колоды</param>
+        public Deck(string name)
+        {
+            DeckName = name;
+            cards = new Card[52];
+
+            int index = 0;
+            foreach (string variant in valueVariants)
+            {
+                foreach (string suit in suitsVariants)
+                {
+                    cards[index++] = new Card(variant, suit);
+                }
+            }
+        }
+    }
+}
