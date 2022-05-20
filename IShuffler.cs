d37d4148e04438005c2c9efa3c40d21cd9af24b8
Artem Levin <leoartemlev@yandex.ru>
2022-05-20 21:38:27 +0500

    Реализовал фабрику для создания перетасовщика

---
+++ IShuffler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Shuffle
+{
+    public interface IShuffler
+    {
+        public void Shuffle(ref Deck.Card[] cards);
+    }
+}
---
+++ SimpleShuffler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Deck;
+
+namespace TestTask.Shuffle
+{
+    public class SimpleShuffler : IShuffler
+    {
+        public void Shuffle(ref Card[] cards)
+        {
+            var random = new Random();
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                var randomIndex = random.Next(cards.Length);
+                var tmp = cards[randomIndex];
+                cards[randomIndex] = cards[i];
+                cards[i] = tmp;
+            }
+        }
+    }
+}
